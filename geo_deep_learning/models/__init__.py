5f538ff6a0ac53e40908409b4e298cfce616f6ec
valhassan <victor.alhassan@canada.ca>
2025-08-27 18:21:58 -0400

    Add installation instructions for MultiScaleDeformableAttention and CUDA toolkit in the models module documentation.

--- geo_deep_learning/models/__init__.py
+++ geo_deep_learning/models/__init__.py
@@ -1,1 +1,10 @@
 """Models for deep learning."""
+
+# install instructions for MultiScaleDeformableAttention:
+# git clone https://github.com/facebookresearch/dinov3.git
+# cd dinov3/dinov3/eval/segmentation/models/utils/ops
+# pip install .
+
+# install cuda toolkit if not already installed in env:
+# for example, if using conda:
+# conda install cuda-toolkit=12.1 -c nvidia
