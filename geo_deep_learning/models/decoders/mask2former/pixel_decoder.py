c9e6f16932774a5b605b3abfac53d64b4ae74d2e
valhassan <victor.alhassan@canada.ca>
2025-08-28 16:10:42 -0400

    Add MSDeformAttnPixelDecoder and associated transformer encoder classes for enhanced segmentation capabilities in Mask2Former architecture.

---
+++ geo_deep_learning/models/decoders/mask2former/pixel_decoder.py
@@ -0,0 +1,466 @@
+"""Mask2Former pixel decoder."""
+
+from collections.abc import Callable
+
+import numpy as np
+import torch
+from torch import nn
+from torch.amp import autocast
+from torch.nn import functional as fn
+from torch.nn.init import normal_
+
+from geo_deep_learning.models.utils import MSDeformAttn
+
+from .utils import (
+    Conv2d,
+    PositionEmbeddingSine,
+    _get_activation_fn,
+    _get_clones,
+    c2_xavier_fill,
+    get_norm,
+)
+
+
+class MSDeformAttnTransformerEncoderOnly(nn.Module):
+    """MSDeformAttnTransformerEncoderOnly."""
+
+    def __init__(  # noqa: PLR0913
+        self,
+        d_model: int = 256,
+        nhead: int = 8,
+        num_encoder_layers: int = 6,
+        dim_feedforward: int = 1024,
+        dropout: float = 0.1,
+        activation: str = "relu",
+        num_feature_levels: int = 4,
+        enc_n_points: int = 4,
+    ) -> None:
+        """Initialize MSDeformAttnTransformerEncoderOnly."""
+        super().__init__()
+
+        self.d_model = d_model
+        self.nhead = nhead
+
+        encoder_layer = MSDeformAttnTransformerEncoderLayer(
+            d_model,
+            dim_feedforward,
+            dropout,
+            activation,
+            num_feature_levels,
+            nhead,
+            enc_n_points,
+        )
+        self.encoder = MSDeformAttnTransformerEncoder(encoder_layer, num_encoder_layers)
+
+        self.level_encoding = nn.Parameter(torch.Tensor(num_feature_levels, d_model))
+
+        self._reset_parameters()
+
+    def _reset_parameters(self) -> None:
+        """Reset parameters."""
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+        for m in self.modules():
+            if isinstance(m, MSDeformAttn):
+                m._reset_parameters()  # noqa: SLF001
+        normal_(self.level_encoding)
+
+    def get_valid_ratio(self, mask: torch.Tensor) -> torch.Tensor:
+        """Get valid ratio."""
+        _, h, w = mask.shape
+        valid_h = torch.sum(~mask[:, :, 0], 1)
+        valid_w = torch.sum(~mask[:, 0, :], 1)
+        valid_ratio_h = valid_h.float() / h
+        valid_ratio_w = valid_w.float() / w
+        return torch.stack([valid_ratio_w, valid_ratio_h], -1)
+
+    def forward(
+        self,
+        srcs: list[torch.Tensor],
+        pos_embeds: list[torch.Tensor],
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """Forward pass."""
+        masks = [
+            torch.zeros(
+                (x.size(0), x.size(2), x.size(3)),
+                device=x.device,
+                dtype=torch.bool,
+            )
+            for x in srcs
+        ]
+        # prepare input for encoder
+        src_flatten = []
+        mask_flatten = []
+        lvl_pos_embed_flatten = []
+        spatial_shapes = []
+        for lvl, (src, mask, pos_embed) in enumerate(
+            zip(srcs, masks, pos_embeds, strict=False),
+        ):
+            bs, c, h, w = src.shape
+            spatial_shape = (h, w)
+            spatial_shapes.append(spatial_shape)
+            src = src.flatten(2).transpose(1, 2)  # noqa: PLW2901
+            mask = mask.flatten(1)  # noqa: PLW2901
+            pos_embed = pos_embed.flatten(2).transpose(1, 2)  # noqa: PLW2901
+            lvl_pos_embed = pos_embed + self.level_encoding[lvl].view(1, 1, -1)
+            lvl_pos_embed_flatten.append(lvl_pos_embed)
+            src_flatten.append(src)
+            mask_flatten.append(mask)
+        src_flatten = torch.cat(src_flatten, 1)
+        mask_flatten = torch.cat(mask_flatten, 1)
+        lvl_pos_embed_flatten = torch.cat(lvl_pos_embed_flatten, 1)
+        spatial_shapes = torch.as_tensor(
+            spatial_shapes,
+            dtype=torch.long,
+            device=src_flatten.device,
+        )
+        level_start_index = torch.cat(
+            (spatial_shapes.new_zeros((1,)), spatial_shapes.prod(1).cumsum(0)[:-1]),
+        )
+        valid_ratios = torch.stack([self.get_valid_ratio(m) for m in masks], 1)
+
+        # encoder
+        memory = self.encoder(
+            src_flatten,
+            spatial_shapes,
+            level_start_index,
+            valid_ratios,
+            lvl_pos_embed_flatten,
+            mask_flatten,
+        )
+
+        return memory, spatial_shapes, level_start_index
+
+
+class MSDeformAttnTransformerEncoderLayer(nn.Module):
+    """MSDeformAttnTransformerEncoderLayer."""
+
+    def __init__(  # noqa: PLR0913
+        self,
+        d_model: int = 256,
+        d_ffn: int = 1024,
+        dropout: float = 0.1,
+        activation: str = "relu",
+        n_levels: int = 4,
+        n_heads: int = 8,
+        n_points: int = 4,
+    ) -> None:
+        """Initialize MSDeformAttnTransformerEncoderLayer."""
+        super().__init__()
+
+        # self attention
+        self.self_attn = MSDeformAttn(d_model, n_levels, n_heads, n_points)
+        self.dropout1 = nn.Dropout(dropout)
+        self.norm1 = nn.LayerNorm(d_model)
+
+        # ffn
+        self.linear1 = nn.Linear(d_model, d_ffn)
+        self.activation = _get_activation_fn(activation)
+        self.dropout2 = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(d_ffn, d_model)
+        self.dropout3 = nn.Dropout(dropout)
+        self.norm2 = nn.LayerNorm(d_model)
+
+    @staticmethod
+    def with_pos_embed(tensor: torch.Tensor, pos: torch.Tensor) -> torch.Tensor:
+        """With pos embed."""
+        return tensor if pos is None else tensor + pos
+
+    def forward_ffn(self, src: torch.Tensor) -> torch.Tensor:
+        """Forward ffn."""
+        src2 = self.linear2(self.dropout2(self.activation(self.linear1(src))))
+        src = src + self.dropout3(src2)
+        return self.norm2(src)
+
+    def forward(  # noqa: PLR0913
+        self,
+        src: torch.Tensor,
+        pos: torch.Tensor,
+        reference_points: torch.Tensor,
+        spatial_shapes: torch.Tensor,
+        level_start_index: torch.Tensor,
+        padding_mask: torch.Tensor | None = None,
+    ) -> torch.Tensor:
+        """Forward pass."""
+        # self attention
+        src2 = self.self_attn(
+            self.with_pos_embed(src, pos),
+            reference_points,
+            src,
+            spatial_shapes,
+            level_start_index,
+            padding_mask,
+        )
+        src = src + self.dropout1(src2)
+        src = self.norm1(src)
+
+        # ffn
+        return self.forward_ffn(src)
+
+
+class MSDeformAttnTransformerEncoder(nn.Module):
+    """MSDeformAttnTransformerEncoder."""
+
+    def __init__(self, encoder_layer: nn.Module, num_layers: int) -> None:
+        """Initialize MSDeformAttnTransformerEncoder."""
+        super().__init__()
+        self.layers = _get_clones(encoder_layer, num_layers)
+        self.num_layers = num_layers
+
+    @staticmethod
+    def get_reference_points(
+        spatial_shapes: torch.Tensor,
+        valid_ratios: torch.Tensor,
+        device: torch.device,
+    ) -> torch.Tensor:
+        """Get reference points."""
+        reference_points_list = []
+        for lvl, (H_, W_) in enumerate(spatial_shapes):  # noqa: N806
+            ref_y, ref_x = torch.meshgrid(
+                torch.linspace(0.5, H_ - 0.5, H_, dtype=torch.float32, device=device),
+                torch.linspace(0.5, W_ - 0.5, W_, dtype=torch.float32, device=device),
+            )
+            ref_y = ref_y.reshape(-1)[None] / (valid_ratios[:, None, lvl, 1] * H_)
+            ref_x = ref_x.reshape(-1)[None] / (valid_ratios[:, None, lvl, 0] * W_)
+            ref = torch.stack((ref_x, ref_y), -1)
+            reference_points_list.append(ref)
+        reference_points = torch.cat(reference_points_list, 1)
+        return reference_points[:, :, None] * valid_ratios[:, None]
+
+    def forward(  # noqa: PLR0913
+        self,
+        src: torch.Tensor,
+        spatial_shapes: torch.Tensor,
+        level_start_index: torch.Tensor,
+        valid_ratios: torch.Tensor,
+        pos: torch.Tensor | None = None,
+        padding_mask: torch.Tensor | None = None,
+    ) -> torch.Tensor:
+        """Forward pass."""
+        output = src
+        reference_points = self.get_reference_points(
+            spatial_shapes,
+            valid_ratios,
+            device=src.device,
+        )
+        for _, layer in enumerate(self.layers):
+            output = layer(
+                output,
+                pos,
+                reference_points,
+                spatial_shapes,
+                level_start_index,
+                padding_mask,
+            )
+
+        return output
+
+
+class MSDeformAttnPixelDecoder(nn.Module):
+    """MSDeformAttnPixelDecoder."""
+
+    def __init__(  # noqa: PLR0913
+        self,
+        input_shape: dict[
+            str,
+            tuple[int],
+        ],  # ShapeSpec: [channels, height, width, stride]
+        *,
+        transformer_dropout: float,
+        transformer_nheads: int,
+        transformer_dim_feedforward: int,
+        transformer_enc_layers: int,
+        conv_dim: int,
+        mask_dim: int,
+        norm: str | Callable | None = None,
+        # deformable transformer encoder args
+        transformer_in_features: list[str],
+        common_stride: int,
+    ) -> None:
+        """
+        NOTE: this interface is experimental.
+
+        Args:
+            input_shape: shapes (channels and stride) of the input features
+            transformer_dropout: dropout probability in transformer
+            transformer_nheads: number of heads in transformer
+            transformer_dim_feedforward: dimension of feedforward network
+            transformer_enc_layers: number of transformer encoder layers
+            conv_dim: number of output channels for the intermediate conv layers.
+            mask_dim: number of output channels for the final conv layer.
+            norm (str or callable): normalization for all conv layers
+            common_stride: stride of the common feature maps
+            transformer_in_features: list of input feature names
+
+        """
+        super().__init__()
+        transformer_input_shape = {
+            k: v for k, v in input_shape.items() if k in transformer_in_features
+        }
+        input_shape = sorted(input_shape.items(), key=lambda x: x[1][-1])
+        self.in_features = [k for k, v in input_shape]
+        self.feature_strides = [v[-1] for k, v in input_shape]
+        self.feature_channels = [v[0] for k, v in input_shape]
+
+        transformer_input_shape = sorted(
+            transformer_input_shape.items(),
+            key=lambda x: x[1][-1],
+        )
+        self.transformer_in_features = [k for k, v in transformer_input_shape]
+        transformer_in_channels = [v[0] for k, v in transformer_input_shape]
+        self.transformer_feature_strides = [v[-1] for k, v in transformer_input_shape]
+
+        self.transformer_num_feature_levels = len(self.transformer_in_features)
+        if self.transformer_num_feature_levels > 1:
+            input_proj_list = [
+                nn.Sequential(
+                    nn.Conv2d(in_channels, conv_dim, kernel_size=1),
+                    nn.GroupNorm(32, conv_dim),
+                )
+                for in_channels in transformer_in_channels[::-1][:-1]
+            ]
+            self.input_convs = nn.ModuleList(input_proj_list)
+        else:
+            self.input_convs = nn.ModuleList(
+                [
+                    nn.Sequential(
+                        nn.Conv2d(transformer_in_channels[-1], conv_dim, kernel_size=1),
+                        nn.GroupNorm(32, conv_dim),
+                    ),
+                ],
+            )
+
+        for proj in self.input_convs:
+            nn.init.xavier_uniform_(proj[0].weight, gain=1)
+            nn.init.constant_(proj[0].bias, 0)
+
+        self.encoder = MSDeformAttnTransformerEncoderOnly(
+            d_model=conv_dim,
+            dropout=transformer_dropout,
+            nhead=transformer_nheads,
+            dim_feedforward=transformer_dim_feedforward,
+            num_encoder_layers=transformer_enc_layers,
+            num_feature_levels=self.transformer_num_feature_levels,
+        )
+        n_steps = conv_dim // 2
+        self.pe_layer = PositionEmbeddingSine(n_steps, normalize=True)
+
+        self.mask_dim = mask_dim
+        # use 1x1 conv instead
+        self.mask_feature = Conv2d(
+            conv_dim,
+            mask_dim,
+            kernel_size=1,
+            stride=1,
+            padding=0,
+        )
+        c2_xavier_fill(self.mask_feature)
+
+        self.maskformer_num_feature_levels = 3  # always use 3 scales
+        self.common_stride = common_stride
+
+        # extra fpn levels
+        stride = min(self.transformer_feature_strides)
+        self.num_fpn_levels = int(np.log2(stride) - np.log2(self.common_stride))
+
+        lateral_convs = []
+        output_convs = []
+
+        use_bias = norm == ""
+        for _, in_channels in enumerate(self.feature_channels[:1]):
+            lateral_norm = get_norm(norm, conv_dim)
+            output_norm = get_norm(norm, conv_dim)
+
+            lateral_conv = Conv2d(
+                in_channels,
+                conv_dim,
+                kernel_size=1,
+                bias=use_bias,
+                norm=lateral_norm,
+            )
+            output_conv = Conv2d(
+                conv_dim,
+                conv_dim,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+                bias=use_bias,
+                norm=output_norm,
+                activation=fn.relu,
+            )
+            c2_xavier_fill(lateral_conv)
+            c2_xavier_fill(output_conv)
+
+            lateral_convs.append(lateral_conv)
+            output_convs.append(output_conv)
+        # Place convs into top-down order (from low to high resolution)
+        # to make the top-down computation in forward clearer.
+        self.lateral_convs = nn.ModuleList(lateral_convs[::-1])
+        self.output_convs = nn.ModuleList(output_convs[::-1])
+
+    @autocast(device_type="cuda", enabled=False)
+    def forward_features(
+        self,
+        features: dict[str, torch.Tensor],
+    ) -> tuple[torch.Tensor, torch.Tensor, list[torch.Tensor]]:
+        """Forward features."""
+        srcs = []
+        pos = []
+        # Reverse feature maps into top-down order (from low to high resolution)
+        for idx, f in enumerate(self.transformer_in_features[::-1][:-1]):
+            x = features[f].float()  # deformable detr does not support half precision
+            srcs.append(self.input_convs[idx](x))
+            pos.append(self.pe_layer(x))
+
+        y, spatial_shapes, level_start_index = self.encoder(srcs, pos)
+        bs = y.shape[0]
+
+        split_size_or_sections = [None] * self.transformer_num_feature_levels
+        for i in range(self.transformer_num_feature_levels):
+            if i < self.transformer_num_feature_levels - 1:
+                split_size_or_sections[i] = (
+                    level_start_index[i + 1] - level_start_index[i]
+                )
+            else:
+                split_size_or_sections[i] = y.shape[1] - level_start_index[i]
+        y = torch.split(y, split_size_or_sections, dim=1)
+
+        out = []
+        multi_scale_features = []
+        num_cur_levels = 0
+        for i, z in enumerate(y):
+            out.append(
+                z.transpose(1, 2).view(
+                    bs,
+                    -1,
+                    spatial_shapes[i][0],
+                    spatial_shapes[i][1],
+                ),
+            )
+
+        # append `out` with extra FPN levels
+        # Reverse feature maps into top-down order (from low to high resolution)
+        for idx, f in enumerate(
+            self.in_features[0],
+        ):
+            x = features[f].float()
+            lateral_conv = self.lateral_convs[idx]
+            output_conv = self.output_convs[idx]
+            cur_fpn = lateral_conv(x)
+            # Following FPN implementation, we use nearest upsampling here
+            y = cur_fpn + fn.interpolate(
+                out[-1],
+                size=cur_fpn.shape[-2:],
+                mode="bilinear",
+                align_corners=False,
+            )
+            y = output_conv(y)
+            out.append(y)
+
+        for o in out:
+            if num_cur_levels < self.maskformer_num_feature_levels:
+                multi_scale_features.append(o)
+                num_cur_levels += 1
+
+        return self.mask_feature(out[-1]), out[0], multi_scale_features
