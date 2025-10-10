0d17c5377d8b0aba348578e7cc5c9a62946566bb
valhassan <victor.alhassan@canada.ca>
2025-10-10 14:44:52 -0400

    Add workflow instructions for installing MultiScaleDeformableAttention using conda, including environment setup and package installation steps.

--- geo_deep_learning/models/__init__.py
+++ geo_deep_learning/models/__init__.py
@@ -8,3 +8,17 @@
 # install cuda toolkit if not already installed in env:
 # for example, if using conda:
 # conda install cuda-toolkit=12.1 -c nvidia
+
+
+# new workflow to install MultiScaleDeformableAttention (uv + conda)
+
+# source /opt/anaconda3/etc/profile.d/conda.sh
+
+# conda create -n cuda-toolkit python=3.12
+# conda activate cuda-toolkit
+# conda install cuda-toolkit=12.8 -c nvidia
+
+# conda activate cuda-toolkit
+# uv pip install --no-build-isolation \
+#   --python /home/valhassa/Projects/geo-deep-learning/.venv/bin/python \
+#   ~/Projects/dinov3/dinov3/eval/segmentation/models/utils/ops
