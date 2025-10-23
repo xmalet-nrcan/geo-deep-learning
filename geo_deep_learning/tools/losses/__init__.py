abbce9420bf67fc5e6bee7d43bafd02c96b18163
valhassan <victor.alhassan@canada.ca>
2025-10-23 14:14:39 -0400

    Remove unused loss function imports and clean up __all__ declaration in losses module for improved clarity and maintainability.

--- geo_deep_learning/tools/losses/__init__.py
+++ geo_deep_learning/tools/losses/__init__.py
@@ -1,17 +1,1 @@
 """Loss functions."""
-
-from .loss_utils import (
-    dice_loss_for_matching,
-    dice_loss_with_num_masks,
-    get_src_permutation_idx,
-    get_tgt_permutation_idx,
-    sigmoid_focal_loss,
-)
-
-__all__ = [
-    "dice_loss_for_matching",
-    "dice_loss_with_num_masks",
-    "get_src_permutation_idx",
-    "get_tgt_permutation_idx",
-    "sigmoid_focal_loss",
-]
