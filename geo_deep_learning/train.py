87badda5c82cb7f961ae2e57d15b3c2f8c72383f
Xavier Malet <xavier.malet@nrcan-rncan.gc.ca>
2025-10-27 17:02:29 -0400

    Implement custom LightningCLI and MLFlowLogger for enhanced testing and logging

--- geo_deep_learning/train.py
+++ geo_deep_learning/train.py
@@ -1,13 +1,13 @@
 """Train model with Lightning CLI."""
 
 import logging
+from typing import Any
 
-from lightning.pytorch import seed_everything
+from lightning.pytorch import Trainer, seed_everything
 from lightning.pytorch.cli import ArgsType, LightningCLI
-
+from lightning.pytorch.loggers import MLFlowLogger
+from lightning.pytorch.callbacks import ModelCheckpoint
 from configs import logging_config  # noqa: F401
-from geo_deep_learning.tools.mlflow_logger import LoggerSaveConfigCallback
-
 
 def safe_name(name: str) -> str:
     """Replace invalid MLflow characters in artifact or run names."""
@@ -23,11 +23,66 @@ def safe_name(name: str) -> str:
 logger = logging.getLogger(__name__)
 
 
+class TestMLFlowLogger(MLFlowLogger):
+    """Custom MLFlowLogger that prevents hyperparameter logging during test."""
+
+    def __init__(self, *args: object, **kwargs: object) -> None:
+        """Initialize TestMLFlowLogger."""
+        super().__init__(*args, **kwargs)
+
+    def log_hyperparams(self, params: dict[str, Any]) -> None:
+        """Override to prevent hyperparameter logging during test."""
+
+
+class GeoDeepLearningCLI(LightningCLI):
+    """Custom LightningCLI."""
+
+    def after_fit(self) -> None:
+        """Log test metrics."""
+        if self.trainer.is_global_zero:
+            test_dataloader = self.datamodule.test_dataloader()
+            if test_dataloader is None:
+                logger.warning("No test dataloader found.")
+                return
+            best_model_path = self.trainer.checkpoint_callback.best_model_path
+
+            try:
+                test_logger = TestMLFlowLogger(
+                    experiment_name=safe_name(self.trainer.logger._experiment_name),
+                    run_name=safe_name(str(self.trainer.logger._run_name)),
+                    run_id=safe_name(str(self.trainer.logger.run_id)),
+                    save_dir=self.trainer.logger.save_dir,
+                )
+            except Exception as e:
+                logger.warning(f"Failed to create TestMLFlowLogger: {e}. Using original logger.")
+                test_logger = self.trainer.logger
+                test_logger.name = safe_name('test_logger_'+test_logger.name)
+
+            test_trainer = Trainer(
+                devices=1,
+                accelerator="auto",
+                strategy="auto",
+                logger=test_logger,
+            )
+            best_model = self.model.__class__.load_from_checkpoint(
+                best_model_path,
+                weights_from_checkpoint_path=None,
+                strict=True,
+            )
+            test_trainer.test(
+                model=best_model,
+                dataloaders=test_dataloader,
+            )
+            self.trainer.logger.log_hyperparams({"best_model_path": safe_name(best_model_path)})
+            logger.info("Test metrics logged successfully to all loggers.")
+        self.trainer.strategy.barrier()
+
+
 def main(args: ArgsType = None) -> None:
     """Run the main training pipeline."""
     seed_everything(42, workers=True)
-    cli = LightningCLI(
-        save_config_callback=LoggerSaveConfigCallback,
+    cli = GeoDeepLearningCLI(
+#        save_config_callback=LoggerSaveConfigCallback,
         save_config_kwargs={"overwrite": True},
         parser_kwargs={"parser_mode": "omegaconf"},
         auto_configure_optimizers=False,
@@ -36,13 +91,6 @@ def main(args: ArgsType = None) -> None:
 
 
     if cli.trainer.is_global_zero:
-        logger.info(
-            "Best model path: %s",
-            cli.trainer.checkpoint_callback.best_model_path,
-        )
-        cli.trainer.logger.log_hyperparams(
-            {"best_model_path": cli.trainer.checkpoint_callback.best_model_path},
-        )
         logger.info("Done!")
 
 
