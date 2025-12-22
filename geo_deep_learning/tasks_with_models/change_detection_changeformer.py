acbe1efac7f125d3e446f3b8d43717a15415ca70
Xavier Malet <xavier.malet@nrcan-rncan.gc.ca>
2025-12-22 10:35:41 -0500

    minor update on changeformer task.

--- geo_deep_learning/tasks_with_models/change_detection_changeformer.py
+++ geo_deep_learning/tasks_with_models/change_detection_changeformer.py
@@ -108,7 +108,7 @@ class ChangeDetectionChangeFormer(LightningModule):
         self.weights_from_checkpoint_path = weights_from_checkpoint_path
 
         self.class_colors = class_colors
-        self.threshold = 0.5
+        self.threshold = 0.3
 
         self.changed_num_classes = num_classes + 1 if num_classes == 1 else num_classes
         self.labels = (
@@ -147,9 +147,9 @@ class ChangeDetectionChangeFormer(LightningModule):
         num_classes = self.num_classes if self.num_classes > 1 else 2
         task_type = "multiclass" if num_classes > 2 else "binary"
         if num_classes == 2:
-            self.train_iou = BinaryJaccardIndex(threshold=0.5)
-            self.val_iou = BinaryJaccardIndex(threshold=0.5)
-            self.test_iou = BinaryJaccardIndex(threshold=0.5)
+            self.train_iou = BinaryJaccardIndex(threshold=self.threshold )
+            self.val_iou = BinaryJaccardIndex(threshold=self.threshold )
+            self.test_iou = BinaryJaccardIndex(threshold=self.threshold)
         else:
             self.train_iou = JaccardIndex(task=task_type, num_classes=num_classes)
             self.val_iou = JaccardIndex(task=task_type, num_classes=num_classes)
