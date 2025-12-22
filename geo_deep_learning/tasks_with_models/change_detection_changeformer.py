0d26634b98a151bc8fb052341d3dfdfb45d2deec
Xavier Malet <xavier.malet@nrcan-rncan.gc.ca>
2025-12-22 15:34:49 -0500

    Refactor ChangeFormer: replace '|' with newline in sample_name for improved readability

--- geo_deep_learning/tasks_with_models/change_detection_changeformer.py
+++ geo_deep_learning/tasks_with_models/change_detection_changeformer.py
@@ -506,7 +506,7 @@ class ChangeDetectionChangeFormer(LightningModule):
                     image=image[[2,3,4], :, :],
                     mask=mask_batch[i],
                     prediction=torch.argmax(outputs[i], dim=0),
-                    sample_name=image_name,
+                    sample_name=image_name.replace('|','\n'),
                     num_classes=self.num_classes,
                     class_colors=self.class_colors,
                 )
